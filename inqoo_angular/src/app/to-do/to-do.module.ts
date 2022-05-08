b4b157dc75cd8ec76c438fe29f7c10c3209bb9c6
Michał Witek <michal.witek.katowice@gmail.com>
2022-05-08 08:25:40 +0200

    add todo page

---
+++ inqoo_angular/src/app/to-do/to-do.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToDoPageComponent } from './pages/to-do-page/to-do-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ToDoPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ToDoModule { }
