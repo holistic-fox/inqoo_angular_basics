a4802762a757a9efda0943de0de4c76f061856ff
Michał Witek <michal.witek.katowice@gmail.com>
2022-05-08 14:13:42 +0200

    add validation errors display

--- inqoo_angular/src/app/to-do/components/to-do-list-item-creator/to-do-list-item-creator.component.ts
+++ inqoo_angular/src/app/to-do/components/to-do-list-item-creator/to-do-list-item-creator.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -6,22 +6,36 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
   templateUrl: './to-do-list-item-creator.component.html',
   styleUrls: ['./to-do-list-item-creator.component.scss']
 })
-export class ToDoListItemCreatorComponent {
+export class ToDoListItemCreatorComponent implements OnInit {
 
   @Output() onItemCreate = new EventEmitter<string>();
 
+  ngOnInit() {
+    console.log(this.itemCreator)
+  }
+
   itemCreator = new FormGroup({
     itemLabel: new FormControl('', [
+      Validators.required,
       Validators.minLength(3),
       Validators.pattern('^[a-zA-Z\\s]*$')
     ])
   });
 
   onItemSubmit = () => {
-    console.log(this.itemCreator)
     if(this.itemCreator.valid) {
       this.onItemCreate.emit(this.itemCreator.value.itemLabel);
       this.itemCreator.reset();
     }
   }
+
+  hasRequiredError = (): boolean =>
+    this.itemCreator?.controls['itemLabel'].errors?.hasOwnProperty('required')
+      && this.itemCreator?.controls['itemLabel'].touched ? true : false;
+
+  hasMinLengthError = (): boolean =>
+    this.itemCreator?.controls['itemLabel'].errors?.hasOwnProperty('minlength') ? true : false;
+
+  hasMinPatternError = (): boolean =>
+    this.itemCreator?.controls['itemLabel'].errors?.hasOwnProperty('pattern') ? true : false;
 }
