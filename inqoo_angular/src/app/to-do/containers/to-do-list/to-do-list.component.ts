4ea9b5ef304e0781fa1773eb2004bab57e73a435
Michał Witek <michal.witek.katowice@gmail.com>
2022-05-08 12:59:55 +0200

    delete items

--- inqoo_angular/src/app/to-do/containers/to-do-list/to-do-list.component.ts
+++ inqoo_angular/src/app/to-do/containers/to-do-list/to-do-list.component.ts
@@ -9,15 +9,25 @@ import {ToDoListMock} from "../../mocks/to-do-list.mock";
 })
 export class ToDoListComponent {
 
-  list: ToDoItem[] = ToDoListMock;
+  list: ToDoItem[] = [];
+  counterId: number = 0;
 
   addToDoItem = (label: string) => this.list.push({
     label,
     isDone: false,
-    id: this.list.length
+    id: this.addCounterToItem()
   })
+
+  addCounterToItem = () => {
+    this.counterId = this.counterId + 1;
+    return this.counterId;
+  }
 
   // onItemDone = (id: number) => this.list = this.list.map(item => item.id === id ? {...item, isDone: true} : item);
   onItemDone = (id: number) => this.list[this.list.findIndex(item => item.id === id)].isDone = true;
+  onItemDelete = (id: number) => {
+    this.list = this.list.filter(item => item.id !== id);
+    // this.list = this.list.map((item, index) => ({...item, id: index}));
+  }
 
 }
